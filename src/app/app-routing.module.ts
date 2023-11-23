5a4274187f56279a2e633660730c75c5f5d5826d
Nizma8 <nizmarahman98@gmail.com>
2023-11-23 10:55:41 +0530

    e-commerce-angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllProductsComponent } from './all-products/all-products.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ViewProductsComponent } from './view-products/view-products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AllProductsComponent },
+  {
+    path: 'user/login',
+    component: LoginComponent,
+  },
+  {
+    path: 'user/register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'wishlist',
+    component: WishlistComponent,
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
+  },
+  {
+    path: 'user/checkout',
+    component: CheckoutComponent,
+  },
+  {
+    path: 'product/view/:id',
+    component: ViewProductsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
